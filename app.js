e2e93cae4522884cdfe1bdc89e545bb611f7007a
Maxim Nesterov <nesmax.jp01@gmail.com>
2024-01-02 13:24:46 +0900

    Adding health check path endpoint

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// PGPASSWORD=3S6HlpPalbLdhoETPlxZcA04kMY1bTgz psql -h dpg-cm9ohm0cmk4c739fhml0-a.oregon-postgres.render.com -U activities_user activity_database_njvt
+// 
+
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
-
+app.get("/health", (req, res) => { 
+  res.sendStatus(200); 
+}); 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
